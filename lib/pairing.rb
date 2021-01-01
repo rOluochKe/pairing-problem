c705184ed67bc8d15b68372ecd760f62ece99e26
Raymond Oluoch <rasholuoch@gmail.com>
2021-01-01 11:30:58 +0300

    adding input values

--- lib/pairing.rb
+++ lib/pairing.rb
@@ -1,12 +1,14 @@
-def brute_force_pair_sums(target, int_array)
+def brute_force_pair_sums(n, int_array)
   a = Array(0..int_array.size - 1)
   a.each do |i|
     b = Array(i..int_array.size - 1)
     b.each do |k|
-      return [int_array[i], int_array[k]] if target == int_array[i] + int_array[k]
+      return [int_array[i], int_array[k]] if n == int_array[i] + int_array[k]
     end
   end
   'no possible pairs'
 end
 
-puts brute_force_pair_sums
+n = 8
+int_array = [4, 5, 5, 5, 5, 5 ]
+puts brute_force_pair_sums(n, int_array)
